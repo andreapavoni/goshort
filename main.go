a885790ae67d0046eeb2205b35cc0a35c69fd695
Andrea Pavoni <andrea.pavoni@gmail.com>
2013-05-02 15:54:16 +0200

    add initial working code

--- main.go
+++ main.go
@@ -1,9 +1,55 @@
 package main
 
 import (
-  "./goshort"
+  "github.com/hoisie/web"
+	"html/template"
+  "fmt"
 )
 
+var model = NewRedisModel("localhost:6379", "", int64(-1))
 
 func main() {
+  web.Get("/", handleIndex)
+  web.Post("/", handleCreate)
+  web.Get("/(.*)", handleShow)
+
+  web.Run("localhost:9999")
+}
+
+type Msg struct {
+	Text string
+}
+
+func renderTemplate(ctx *web.Context, tmpl string, msg Msg) {
+	t, _ := template.ParseFiles("views/" + tmpl + ".html")
+  t.Execute(ctx.ResponseWriter, msg)
+}
+
+func handleIndex(ctx *web.Context) {
+  renderTemplate(ctx, "index", Msg{""})
+}
+
+func handleCreate(ctx *web.Context) {
+  msg := Msg{""}
+  url := ctx.Params["url"]
+  short, err := model.Create(url)
+
+  if err == nil {
+    msg = Msg{fmt.Sprintf("Saved URL %s with Id %s", short.Url, short.Id)}
+  } else {
+    msg = Msg{fmt.Sprintf("Error: %s", err)}
+  }
+
+  renderTemplate(ctx, "index", msg)
+}
+
+func handleShow(ctx *web.Context, id string) {
+  res, err := model.FindBy("id", id)
+
+  if err == nil {
+    ctx.Redirect(302, res.Url)
+    return
+  } else {
+    renderTemplate(ctx, "index", Msg{fmt.Sprintf("Not Found Id %s", id)})
+  }
 }
---
+++ shorturl.go
@@ -0,0 +1,146 @@
+package main
+
+import (
+	"regexp"
+  "fmt"
+  "errors"
+  "github.com/vmihailenco/redis"
+
+	"math/rand"
+	"time"
+
+	"strconv"
+  "strings"
+)
+
+type RedisModel struct {
+  redisPrefix string
+  client *redis.Client
+}
+
+type ShortUrl struct {
+  Id, Url string
+}
+
+// Initializer
+func NewRedisModel(host, password string, db int64) *RedisModel {
+	client := redis.NewTCPClient(host, password, db)
+	defer client.Close()
+
+  return &RedisModel{"goshort", client}
+}
+
+func (d *RedisModel) Create(url string) (*ShortUrl, error) {
+  if !validateUrlFormat(url) {
+    return nil, errors.New("data: Invalid url format")
+  }
+
+  multi, _ := d.client.MultiClient()
+  defer multi.Close()
+
+  id := generateId()
+
+  _, err := multi.Exec(func() {
+    multi.Set(d.redisKey("id", id), url)
+    multi.Set(d.redisKey("url", url), id)
+  })
+
+  if err != nil {
+    return nil, err
+  }
+
+  return &ShortUrl{id, url}, nil
+}
+
+func (d *RedisModel) FindBy(key, value string) (*ShortUrl, error) {
+  val, err := d.getKey(d.redisKey(key, value))
+
+  if err != nil {
+    return nil, err
+  }
+
+  var result ShortUrl
+
+  switch key {
+  case "id":
+    result = ShortUrl{value, val}
+  case "url":
+    result = ShortUrl{val, value}
+  }
+
+  return &result, nil
+}
+
+// RedisModel private methods
+
+func (d *RedisModel) redisKey(key, value string) string {
+  return fmt.Sprintf("%s.%s|%s", d.redisPrefix, key, value)
+}
+
+func (d *RedisModel) getKey(key string) (string, error) {
+  get := d.client.Get(key)
+
+  if get.Err() != nil {
+    return "", get.Err()
+  }
+
+  val := get.Val()
+
+  if val == "" {
+    err := fmt.Sprintf("data: Key not found %s", key)
+    return "", errors.New(err)
+  }
+
+  return val, nil
+}
+
+// Misc utils
+
+func generateId() string {
+	/* check existent ids somewhere (eg: Redis) */
+	return base62(randInt())
+}
+
+func randInt() int {
+	seed := time.Now().UTC().UnixNano()
+	rand.Seed(seed)
+
+	val := int(seed)
+	if val < 0 {
+		val = -val
+	}
+
+	return (1 + rand.Intn(val))
+}
+
+func base62(num int) string {
+	const chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+	enc := make([]uint8, len(string(strconv.Itoa(num))))
+
+	if num == 0 {
+		return "0"
+	}
+
+	for i := 0; num > 0; i++ {
+		n := num % 62
+
+		enc[i] = chars[n]
+		num /= 62
+	}
+
+  var n uint8
+  return strings.Trim(string(enc), string(n))
+
+}
+
+func validateUrlFormat(url string) bool {
+	const pattern = "(^$)|(^(http|https)://[a-z0-9]+([-.]{1}[a-z0-9]+)*.[a-z]{2,5}(([0-9]{1,5})?/.*)?$)"
+
+  if url == "" {
+    return false
+  }
+
+	result, _ := regexp.MatchString(pattern, url)
+
+	return result
+}
